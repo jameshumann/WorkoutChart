c58d689f1d45de37ab5b28446d243a580f631129
jameshumann <james.d.humann.civ@mail.mil>
2020-03-10 15:38:55 -0700

    Added rows for pushups an lunges

--- workoutChart.py
+++ workoutChart.py
@@ -1,1 +1,68 @@
-#include math
+#workoutChart.p
+from graphics import *
+import math
+
+print("Hellowortdl!")
+
+def main():
+    docWidth = 1100 #pixels
+    docHeight = 850
+    margin = 50 # pixels, 1/2 inch
+    rowBegin = margin + 50
+    rowEnd = docWidth - margin
+    horizontalArea = rowEnd - rowBegin
+    twoWeekWidth = (horizontalArea/17) * 14
+    rowHeight = 10
+    interRowSpace = 8
+
+    win = GraphWin("canvas", docWidth, docHeight)
+
+    rowTop = rowBegin + 50
+    rowBottom = rowTop + rowHeight
+
+    """ Add rows for pushups """
+    numPushups = 2*31 #per month
+    pd = numPushups / 31.0 # per day
+    p17d = pd * 17  #per 17 days
+    pushupWidth = horizontalArea / p17d
+    inTopRow = math.ceil(pd*14)
+    inBottomRow = numPushups - inTopRow
+    bottomOffset = inTopRow * pushupWidth - twoWeekWidth
+
+    for rect in range(inTopRow):
+        r = Rectangle(Point(rowBegin + pushupWidth*rect, rowTop), Point(rowBegin + pushupWidth*(rect+1), rowBottom))
+        r.draw(win)
+    
+    for rect in range(inBottomRow):
+        r = Rectangle(Point(rowBegin + bottomOffset + pushupWidth*rect, rowTop + 300), Point(rowBegin + bottomOffset + pushupWidth*(rect+1), rowBottom + 300))
+        r.draw(win)
+
+
+    rowTop = rowBottom + interRowSpace
+    rowBottom = rowTop + rowHeight
+
+    print(rowTop)
+    print(rowBottom)
+    """ Add rows for Lunges """
+    numLunges =  31/1.5 #per month
+    numLunges = round(numLunges)
+    pd = numLunges / 31.0 # per day
+    p17d = pd * 17  #per 17 days
+    lungesWidth = horizontalArea / p17d
+    inTopRow = math.ceil(pd*14)
+    inBottomRow = numLunges - inTopRow
+    bottomOffset = inTopRow * lungesWidth - twoWeekWidth
+
+    for rect in range(inTopRow):
+        r = Rectangle(Point(rowBegin + lungesWidth*rect, rowTop), Point(rowBegin + lungesWidth*(rect+1), rowBottom))
+        r.draw(win)
+    
+    for rect in range(inBottomRow):
+        r = Rectangle(Point(rowBegin + bottomOffset + lungesWidth*rect, rowTop + 300), Point(rowBegin + bottomOffset + lungesWidth*(rect+1), rowBottom + 300))
+        r.draw(win)
+
+    win.getMouse()
+    win.close()
+
+main()
+
