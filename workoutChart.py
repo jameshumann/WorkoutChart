bb032dba9eaa470f84854fac52eae2a2776b4b69
James Humann <jamesdhumann@gmail.com>
2020-03-10 20:19:20 -0700

    added row and began labeling, added vertical bars

--- workoutChart.py
+++ workoutChart.py
@@ -8,7 +8,8 @@ def main():
     docWidth = 1100 #pixels
     docHeight = 850
     margin = 50 # pixels, 1/2 inch
-    rowBegin = margin + 50
+    labelWidth = 160
+    rowBegin = margin + labelWidth
     rowEnd = docWidth - margin
     horizontalArea = rowEnd - rowBegin
     twoWeekWidth = (horizontalArea/17) * 14
@@ -16,13 +17,20 @@ def main():
     interRowSpace = 8
 
     win = GraphWin("canvas", docWidth, docHeight)
+    win.setBackground("white")
 
-    rowTop = rowBegin + 50
+    rowTop = margin + 100
     rowBottom = rowTop + rowHeight
 
+    """ Add Vertical lines """
+    for line in [1,2,3,4]:
+        l = Line(Point(rowBegin + line*twoWeekWidth/2, margin),Point(rowBegin + line*twoWeekWidth/2, docHeight - margin))
+        l.setWidth(3)
+        l.draw(win)
+
     """ Add rows for pushups """
-    numPushups = 2*31 #per month
-    pd = numPushups / 31.0 # per day
+    numPushups = 2*30 #per 30 days
+    pd = numPushups / 30.0 # per day
     p17d = pd * 17  #per 17 days
     pushupWidth = horizontalArea / p17d
     inTopRow = math.ceil(pd*14)
@@ -31,19 +39,21 @@ def main():
 
     for rect in range(inTopRow):
         r = Rectangle(Point(rowBegin + pushupWidth*rect, rowTop), Point(rowBegin + pushupWidth*(rect+1), rowBottom))
+        r.setFill("lightgrey")
         r.draw(win)
     
     for rect in range(inBottomRow):
         r = Rectangle(Point(rowBegin + bottomOffset + pushupWidth*rect, rowTop + 300), Point(rowBegin + bottomOffset + pushupWidth*(rect+1), rowBottom + 300))
+        r.setFill("lightgrey")
         r.draw(win)
 
 
     rowTop = rowBottom + interRowSpace
     rowBottom = rowTop + rowHeight
     """ Add rows for Lunges """
-    numLunges =  31/1.5 #per month
+    numLunges =  30/1.5 #per month
     numLunges = round(numLunges)
-    pd = numLunges / 31.0 # per day
+    pd = numLunges / 30.0 # per day
     p17d = pd * 17  #per 17 days
     lungesWidth = horizontalArea / p17d
     inTopRow = math.ceil(pd*14)
@@ -52,10 +62,12 @@ def main():
 
     for rect in range(inTopRow):
         r = Rectangle(Point(rowBegin + lungesWidth*rect, rowTop), Point(rowBegin + lungesWidth*(rect+1), rowBottom))
+        r.setFill("lightgrey")
         r.draw(win)
     
     for rect in range(inBottomRow):
         r = Rectangle(Point(rowBegin + bottomOffset + lungesWidth*rect, rowTop + 300), Point(rowBegin + bottomOffset + lungesWidth*(rect+1), rowBottom + 300))
+        r.setFill("lightgrey")
         r.draw(win)
 
 
@@ -63,9 +75,9 @@ def main():
     rowBottom = rowTop + rowHeight
     """ Add rows for PT """
     nameEx = "PT Exercise"
-    numEx =  31*2 #per month
+    numEx =  30*2 #per month
     numEx = round(numEx)
-    pd = numEx / 31.0 # per day
+    pd = numEx / 30.0 # per day
     p17d = pd * 17  #per 17 days
     exWidth = horizontalArea / p17d
     inTopRow = math.ceil(pd*14)
@@ -74,19 +86,21 @@ def main():
 
     for rect in range(inTopRow):
         r = Rectangle(Point(rowBegin + exWidth*rect, rowTop), Point(rowBegin + exWidth*(rect+1), rowBottom))
+        r.setFill("lightgrey")
         r.draw(win)
     
     for rect in range(inBottomRow):
         r = Rectangle(Point(rowBegin + bottomOffset + exWidth*rect, rowTop + 300), Point(rowBegin + bottomOffset + exWidth*(rect+1), rowBottom + 300))
+        r.setFill("lightgrey")
         r.draw(win)
     
     rowTop = rowBottom + interRowSpace
     rowBottom = rowTop + rowHeight
     """ Add rows for cardio """
     nameEx = "Cardio"
-    numEx =  (8/7)*31 #per month
+    numEx =  (8/7)*30 #per month
     numEx = round(numEx)
-    pd = numEx / 31.0 # per day
+    pd = numEx / 30.0 # per day
     p17d = pd * 17  #per 17 days
     exWidth = horizontalArea / p17d
     inTopRow = math.ceil(pd*14)
@@ -95,11 +109,39 @@ def main():
 
     for rect in range(inTopRow):
         r = Rectangle(Point(rowBegin + exWidth*rect, rowTop), Point(rowBegin + exWidth*(rect+1), rowBottom))
+        r.setFill("lightgrey")
         r.draw(win)
     
     for rect in range(inBottomRow):
         r = Rectangle(Point(rowBegin + bottomOffset + exWidth*rect, rowTop + 300), Point(rowBegin + bottomOffset + exWidth*(rect+1), rowBottom + 300))
+        r.setFill("lightgrey")
         r.draw(win)
+
+    rowTop = rowBottom + interRowSpace
+    rowBottom = rowTop + rowHeight
+    """ Add rows for pullups """
+    nameEx = "Pullups x 5"
+    Text(Point((margin + labelWidth/2),(rowTop + interRowSpace*.8)),nameEx).draw(win)
+    numEx =  (1.2)*30 #per month
+    numEx = round(numEx)
+    pd = numEx / 30.0 # per day
+    p17d = pd * 17  #per 17 days
+    exWidth = horizontalArea / p17d
+    inTopRow = math.ceil(pd*14)
+    inBottomRow = numEx - inTopRow
+    bottomOffset = inTopRow * exWidth - twoWeekWidth
+
+    for rect in range(inTopRow):
+        r = Rectangle(Point(rowBegin + exWidth*rect, rowTop), Point(rowBegin + exWidth*(rect+1), rowBottom))
+        r.setFill("lightgrey")
+        r.draw(win)
+    
+    for rect in range(inBottomRow):
+        r = Rectangle(Point(rowBegin + bottomOffset + exWidth*rect, rowTop + 300), Point(rowBegin + bottomOffset + exWidth*(rect+1), rowBottom + 300))
+        r.setFill("lightgrey")
+        r.draw(win)
+
+    
 
     win.getMouse()
     win.close()
