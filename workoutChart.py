b2ce7ad427015d5c200272e8a78c3c2cf6356d4b
James Humann <jamesdhumann@gmail.com>
2020-04-02 20:25:25 -0700

    add day labels

--- workoutChart.py
+++ workoutChart.py
@@ -9,18 +9,40 @@ def main():
     docHeight = 850
     margin = 50 # pixels, 1/2 inch
     labelWidth = 160
+    rowHeight = 15
+    interRowSpace = 8
+    spaceBetweenRowGroups = 300
+
     rowBegin = margin + labelWidth
     rowEnd = docWidth - margin
     horizontalArea = rowEnd - rowBegin
     twoWeekWidth = (horizontalArea/17) * 14
-    rowHeight = 15
-    interRowSpace = 8
+    dayWidth = twoWeekWidth / 14
+    verticalSpace = docHeight - 2*margin
 
     win = GraphWin("canvas", docWidth, docHeight)
     win.setBackground("white")
 
     rowTop = margin + 100
     rowBottom = rowTop + rowHeight
+
+    
+
+    """ Add days """
+    for day in range(31):
+        xpos = rowBegin + (day+1)*dayWidth
+        if(day > 13):
+            xpos = xpos - 14*dayWidth
+
+        ypos = rowTop if (day < 14) else rowTop + spaceBetweenRowGroups
+        
+        l = Line(Point(xpos, ypos - margin*.333), Point(xpos, ypos))
+        l.setWidth(1)
+        l.setOutline("grey")
+        l.draw(win)
+
+        t = Text(Point(xpos - dayWidth*0.3, ypos - margin*0.2), day+1)
+        t.draw(win)
 
     """ Add Vertical lines """
     for line in [1,2,3,4]:
@@ -29,13 +51,16 @@ def main():
         l.draw(win)
 
     """ Add rows for pushups """
+    nameEx = "Pushups x 10"
     numPushups = 2*30 #per 30 days
-    pd = numPushups / 30.0 # per day
+    numPushups = round(numPushups * (31/30)) # make 31 day month
+    pd = numPushups / 31.0 # per day
     p17d = pd * 17  #per 17 days
     pushupWidth = horizontalArea / p17d
     inTopRow = math.ceil(pd*14)
     inBottomRow = numPushups - inTopRow
     bottomOffset = inTopRow * pushupWidth - twoWeekWidth
+    Text(Point((margin + labelWidth/2),(rowTop + interRowSpace*.8)),nameEx).draw(win)
 
     for rect in range(inTopRow):
         r = Rectangle(Point(rowBegin + pushupWidth*rect, rowTop), Point(rowBegin + pushupWidth*(rect+1), rowBottom))
@@ -51,14 +76,16 @@ def main():
     rowTop = rowBottom + interRowSpace
     rowBottom = rowTop + rowHeight
     """ Add rows for Lunges """
-    numLunges =  30/1.5 #per month
-    numLunges = round(numLunges)
-    pd = numLunges / 30.0 # per day
+    nameEx = "Lunges x 10 (40 kg)"
+    numLunges =  20 #per 30-day month
+    numLunges = round(numLunges * (31/30)) # make 31 day month
+    pd = numLunges / 31.0 # per day
     p17d = pd * 17  #per 17 days
     lungesWidth = horizontalArea / p17d
     inTopRow = math.ceil(pd*14)
     inBottomRow = numLunges - inTopRow
     bottomOffset = inTopRow * lungesWidth - twoWeekWidth
+    Text(Point((margin + labelWidth/2),(rowTop + interRowSpace*.8)),nameEx).draw(win)
 
     for rect in range(inTopRow):
         r = Rectangle(Point(rowBegin + lungesWidth*rect, rowTop), Point(rowBegin + lungesWidth*(rect+1), rowBottom))
@@ -75,14 +102,15 @@ def main():
     rowBottom = rowTop + rowHeight
     """ Add rows for PT """
     nameEx = "PT Exercise"
-    numEx =  30*2 #per month
-    numEx = round(numEx)
-    pd = numEx / 30.0 # per day
+    numEx =  30*2 #per 30-day month
+    numEx = round(numEx * (31/30))
+    pd = numEx / 31.0 # per day
     p17d = pd * 17  #per 17 days
     exWidth = horizontalArea / p17d
     inTopRow = math.ceil(pd*14)
     inBottomRow = numEx - inTopRow
     bottomOffset = inTopRow * exWidth - twoWeekWidth
+    Text(Point((margin + labelWidth/2),(rowTop + interRowSpace*.8)),nameEx).draw(win)
 
     for rect in range(inTopRow):
         r = Rectangle(Point(rowBegin + exWidth*rect, rowTop), Point(rowBegin + exWidth*(rect+1), rowBottom))
@@ -99,13 +127,14 @@ def main():
     """ Add rows for cardio """
     nameEx = "Cardio"
     numEx =  (8/7)*30 #per month
-    numEx = round(numEx)
-    pd = numEx / 30.0 # per day
+    numEx = round(numEx * (31/30))
+    pd = numEx / 31.0 # per day
     p17d = pd * 17  #per 17 days
     exWidth = horizontalArea / p17d
     inTopRow = math.ceil(pd*14)
     inBottomRow = numEx - inTopRow
     bottomOffset = inTopRow * exWidth - twoWeekWidth
+    Text(Point((margin + labelWidth/2),(rowTop + interRowSpace*.8)),nameEx).draw(win)
 
     for rect in range(inTopRow):
         r = Rectangle(Point(rowBegin + exWidth*rect, rowTop), Point(rowBegin + exWidth*(rect+1), rowBottom))
@@ -123,8 +152,8 @@ def main():
     nameEx = "Pullups x 5"
     Text(Point((margin + labelWidth/2),(rowTop + interRowSpace*.8)),nameEx).draw(win)
     numEx =  (1.2)*30 #per month
-    numEx = round(numEx)
-    pd = numEx / 30.0 # per day
+    numEx = round(numEx * (31/30))
+    pd = numEx / 31.0 # per day
     p17d = pd * 17  #per 17 days
     exWidth = horizontalArea / p17d
     inTopRow = math.ceil(pd*14)
